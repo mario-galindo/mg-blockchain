053f2713a33a1dbc65aa162f4d2f101b995b5632
mario-galindo <mgalindo9410@gmail.com>
2020-07-24 22:10:10 -0600

    add tests code

---
+++ block.test.js
@@ -0,0 +1,20 @@
+const Block = require("./block");
+
+describe("block", () => {
+  let data, lastBlock, block;
+
+  beforeEach(() => {
+    data = "bar";
+    lastBlock = Block.genesis();
+    block = Block.mineBlock(lastBlock, data);
+  });
+
+  it("sets the `data` to match the input", () => {
+    expect(block.data).toEqual(data);
+  });
+
+  it("sets the `lastHash` to match the hash of the last block", () => {
+    expect("f1r57-h45h").toEqual(lastBlock.hash);
+  });
+
+});
