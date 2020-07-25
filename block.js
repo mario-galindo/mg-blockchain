bee375c684619367d89ab9e6eb393b545f10cf0f
mario-galindo <mgalindo9410@gmail.com>
2020-07-24 21:06:18 -0600

    change hardcode hash to be generate by a new function hash

--- block.js
+++ block.js
@@ -24,9 +24,13 @@ class Block {
   static mineBlock(lastBlock, data) {
     const timestamp = Date.now();
     const lastHash = lastBlock.hash;
-    const hash = "todo-hash";
+    const hash = Block.hash(timestamp, lastHash, data);
 
     return new this(timestamp, lastHash, hash, data);
+  }
+
+  static hash(timestamp, lastHash, data) {
+    return SHA256(`${timestamp}${lastHash}${data}`).toString();
   }
 }
 
