93634e1e99a40495e1a3b2eacae1edaa36c104ea
mario-galindo <mgalindo9410@gmail.com>
2020-07-27 00:54:29 -0600

    add tests for replacement functions

--- blockchain.test.js
+++ blockchain.test.js
@@ -36,4 +36,18 @@ describe("Blockchain", () => {
 
     expect(bc.isValidChain(bc2.chain)).toBe(false);
   });
+
+  it("Replace the chain with a valid chain", () => {
+    bc2.addBlock("goo");
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).toEqual(bc2.chain);
+  });
+
+  it("Does not replace the chain with one or less than or iqual to length", () => {
+    bc.addBlock("foo");
+    bc.replaceChain(bc2.chain);
+
+    expect(bc.chain).not.toEqual(bc2.chain);
+  });
 });
