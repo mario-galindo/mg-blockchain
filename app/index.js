a8c84e4b23e0622aa1b10b83dc6bb5796821e952
mario-galindo <mgalindo9410@gmail.com>
2020-07-27 18:45:36 -0600

    add post http request for mine a block inside a chain

--- app/index.js
+++ app/index.js
@@ -14,4 +14,11 @@ app.get("/blocks", (req, res) => {
   res.json(bc.chain);
 });
 
+app.post("/mine", (req, res) => {
+  const block = bc.addBlock(req.body.data);
+  console.log(`New block added: ${block.toString()}`);
+
+  res.redirect("/blocks");
+});
+
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
